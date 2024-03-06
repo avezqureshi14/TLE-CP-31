3729412afb7c162a36ac1f47fcf2b00438b2448b
avezqureshi <aveznqureshi14@gmail.com>
2024-03-06 16:37:17 +0530

    feat:halloumi added

--- 01-halloumi-boxes.cpp
+++ 01-halloumi-boxes.cpp
@@ -2,8 +2,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isSorted(vector<int> arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        if (isSorted(arr, n) || k > 1)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+    }
     return 0;
 }
